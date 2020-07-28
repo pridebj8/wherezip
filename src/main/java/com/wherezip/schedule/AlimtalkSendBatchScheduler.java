e4441b2b3ec2262d0288edf644aa01ce7b8a5ae2
pridebj88 <pridebj88@gmail.com>
2020-07-28 21:52:30 +0900

    서비스 스케줄러 수정

--- src/main/java/com/wherezip/schedule/AlimtalkSendBatchScheduler.java
+++ src/main/java/com/wherezip/schedule/AlimtalkSendBatchScheduler.java
@@ -1,10 +1,16 @@
 package com.wherezip.schedule;
 
+import com.wherezip.service.AlimtalkSendBatchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class AlimtalkSendBatchScheduler implements Scheduler {
+
+    private final AlimtalkSendBatchService alimtalkSendBatchService;
 
     @Scheduled(initialDelay = 3000, fixedDelay = 3600 * 1000)
     @Override
