370a51def1813a248e710b4c99ad47c405663b00
pridebj88 <pridebj88@gmail.com>
2020-08-04 13:13:08 +0900

    설정 추가

--- src/main/java/com/wherezip/config/Config.java
+++ src/main/java/com/wherezip/config/Config.java
@@ -1,4 +1,20 @@
 package com.wherezip.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Setter
+@Getter
+@ConfigurationProperties(prefix = "wherezip")
 public class Config {
+
+    private String sensApiUrl;
+
+    private String sensServiceId;
+
+    private String sensAccessKeyId;
+
+    private String sensSecretKey;
+
 }
